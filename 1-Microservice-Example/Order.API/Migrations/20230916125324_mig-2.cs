cf9bd3fe85d44e3069b6068b1edeb61f71be508d
mert-celik-1 <mertcelik2399@gmail.com>
2023-09-17 23:41:01 +0300

    microservis örnek

---
+++ 1-Microservice-Example/Order.API/Migrations/20230916125324_mig-2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Order.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalPrice",
+                table: "Orders",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductId",
+                table: "OrderItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "TotalPrice",
+                table: "Orders",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductId",
+                table: "OrderItems",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
---
+++ 1-Microservice-Example/Order.API/Models/Entities/OrderItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Order.API.Models.Entities
+{
+    public class OrderItem
+    {
+        public Guid Id { get; set; }
+        public string ProductId { get; set; }
+        public int Count { get; set; }
+        public decimal Price { get; set; }
+        public Order Order { get; set; }
+    }
+}
